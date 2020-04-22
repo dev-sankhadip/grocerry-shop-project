47d50df6b6b9bf8e6552d49a7fa8515cacfa97bc
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-22 09:31:38 +0530

    feat:(client) dashboard page updated

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -4,8 +4,25 @@ import React from 'react';
 export const Dashboard=(props)=>
 {
     return(
-        <React.Fragment>
-            
-        </React.Fragment>
+        <div className="container p-2">
+            <ul className="nav nav-tabs" id="myTab" role="tablist">
+                <li className="nav-item">
+                    <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Your Expenditure</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Buy Here</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Add Product</a>
+                </li>
+            </ul>
+            <div className="tab-content" id="myTabContent">
+                <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    
+                </div>
+                <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+            </div>
+        </div>
     )
 }
