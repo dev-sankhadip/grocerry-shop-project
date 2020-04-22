3b356cb807044e0f0c21ada834cb61b0f3781623
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-22 09:31:20 +0530

    feat:(client) login page updated

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style/login.css";
 
 export const Login = (props) => {
+  const [name, setName] = useState("");
+  const UpdateName = (e) => {
+    setName(e.target.value);
+  };
+
+  const Submit = () => {
+    console.log(name);
+    props.history.push("/dashboard");
+  };
+
   return (
     <React.Fragment>
       <div className="container p-2">
         <div className="center">
           <div className="mb-2">
-            <select class="form-control form-control-sm">
-              <option>Select</option>
+            <select className="form-control form-control-sm" onChange={UpdateName}>
+              <option value="1">Sankha</option>
+              <option value="2">Arun</option>
+              <option value="3">Tanishq</option>
             </select>
           </div>
-          <button type="submit" class="btn btn-sm btn-primary">
+          <button type="button" className="btn btn-sm btn-primary" onClick={Submit}>
             Submit
           </button>
         </div>
