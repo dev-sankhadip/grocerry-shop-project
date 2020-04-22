6a60189f751628182dc9d925efab31fe7a9a1642
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-22 08:33:52 +0530

    update added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Layout } from './components/layout';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Layout />
     </div>
   );
 }
